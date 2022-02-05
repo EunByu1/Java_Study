7e9a678a956e8c5613db8512b1d29fd8a957384e
은지 <89649741+EunByu1@users.noreply.github.com>
2022-02-05 23:23:08 +0900

    Initial commit

---
+++ src/com/company/Hello.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Hello {
+        public static void main(String[] args) {    // main 메서드의 선언부
+            System.out.println("Hello world.");     // 화면에 글자를 출력한다.
+        }
+}
