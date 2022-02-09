3025dd3b8a9831a54645e1d714a28a3503416459
은지 <89649741+EunByu1@users.noreply.github.com>
2022-02-09 15:15:00 +0900

    22.02.09

---
+++ src/JavaStrandard/com/company/Ch2_CastingEx1.java
@@ -0,0 +1,13 @@
+package JavaStrandard.com.company;
+
+public class Ch2_CastingEx1 {
+
+    public static void main12(String[] args){
+        double d  = 85.4;
+        int score = (int)d;     // 형변환 후에도 피연산자에는 아무런 변화가 없다.
+
+        System.out.println("score = " + score);
+        System.out.println("d = " + d);
+    }
+
+}
---
+++ src/JavaStrandard/com/company/Ch2_FloatToBinEx.java
@@ -0,0 +1,13 @@
+package JavaStrandard.com.company;
+
+public class Ch2_FloatToBinEx {
+
+    public static void main11(String[] args){
+        float f = 9.1234567f;
+        int   i = Float.floatToIntBits(f);
+
+        System.out.printf("%f%n", f);
+        System.out.printf("%X%n", i);      // 16진수로 출력
+    }
+
+}
